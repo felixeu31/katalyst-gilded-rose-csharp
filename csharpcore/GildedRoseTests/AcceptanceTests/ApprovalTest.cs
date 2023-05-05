c17650713ea47fb857cf18983e06e1bd175d3827
Félix Díez Arias <fdiez@tunstall.es>
2023-05-05 07:25:13 +0200

    chore: move tests to better namespace

--- csharpcore/GildedRoseTests/AcceptanceTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/AcceptanceTests/ApprovalTest.cs
@@ -3,10 +3,9 @@ using System.IO;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
-using GildedRoseTests;
 using NUnit.Framework;
 
-namespace csharp
+namespace GildedRoseTests.AcceptanceTests
 {
     [UseReporter(typeof(DiffReporter))]
     [TestFixture]
@@ -15,7 +14,7 @@ namespace csharp
         [Test]
         public void ThirtyDays()
         {
-            
+
             StringBuilder fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
             Console.SetIn(new StringReader("a\n"));
