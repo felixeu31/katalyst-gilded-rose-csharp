4b49eff37a88c6ecb05ee4a75a896dd02ec22ca3
Félix Díez <felixeu31@gmail.com>
2023-05-06 07:49:55 +0200

    test: add all remaining test covering given requirements

--- csharpcore/GildedRoseTests/UnitTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/UnitTests/GildedRoseTest.cs
@@ -33,5 +33,71 @@ namespace GildedRoseTests.UnitTests
             app.UpdateQuality();
             Assert.Equal(8, Items[0].Quality);
         }
+
+
+        [Fact]
+        public void UpdateQuality_WhenAgedBrie_IncreaseQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(11, Items[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_WhenAgedBrieHasMaxQuality_DoesNotIncreaseQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(50, Items[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_WhenLegendaryItem_QualityRemainsTheSame()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(80, Items[0].Quality);
+        }
+
+
+        [Fact]
+        public void UpdateQuality_WhenBackstagePasses_QualityIncreases()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(21, Items[0].Quality);
+        }
+
+
+        [Fact]
+        public void UpdateQuality_WhenBackstagePasses_QualityIncreasesTwice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(22, Items[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_WhenBackstagePasses_QualityIncreasesTriple()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(23, Items[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_WhenBackstagePassesAndSellInDatePassed_QualityDropsTo0()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
     }
 }
---
+++ csharpcore/GildedRoseTests/dev-notes.md
@@ -0,0 +1,13 @@
+﻿## Requirements
+
+	- [OK] At the end of each day our system lowers both values for every item
+	- [OK] The Quality of an item is never negative
+	- [OK] Once the sell by date has passed, Quality degrades twice as fast
+	- [OK] "Aged Brie" actually increases in Quality the older it gets
+	- [OK] The Quality of an item is never more than 50
+	- [OK] "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- [OK] "Sulfuras" is alegendary item and as such its Quality is 80 and it never alters.
+	- [OK] "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	- [OK] "Backstage passes", Quality increases by 2 when there are 10 days or less 
+	- [OK] "Backstage passes", Quality increases  by 3 when there are 5 days or less
+	- [OK] "Backstage passes", Quality drops to 0 after the concert
