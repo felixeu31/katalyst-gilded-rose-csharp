3bb13e390aef193b47b4b075331313c9417bc486
Félix Díez <felixeu31@gmail.com>
2023-05-06 07:59:03 +0200

    test: sellIn date requirements tests

--- csharpcore/GildedRoseTests/UnitTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/UnitTests/GildedRoseTest.cs
@@ -9,7 +9,7 @@ namespace GildedRoseTests.UnitTests
         [Fact]
         public void UpdateQuality_WhenSimpleItemWithQualityOver0_DecreaseQuality()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Bread", SellIn = 10, Quality = 10 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = 10, Quality = 10 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(9, Items[0].Quality);
@@ -18,7 +18,7 @@ namespace GildedRoseTests.UnitTests
         [Fact]
         public void UpdateQuality_WhenSimpleItemWithQualityEqualsTo0_MaintainsQualityIn0()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Bread", SellIn = 10, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = 10, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(0, Items[0].Quality);
@@ -28,7 +28,7 @@ namespace GildedRoseTests.UnitTests
         [Fact]
         public void UpdateQuality_WhenSimpleItemWithQualityEqualsOver0AndSellInDatePassed_DecreaseQualityTwice()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Bread", SellIn = -1, Quality = 10 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = -1, Quality = 10 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(8, Items[0].Quality);
@@ -98,6 +98,40 @@ namespace GildedRoseTests.UnitTests
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert")]
+        [InlineData("Aged Brie")]
+        [InlineData("Elixir of the Mongoose")]
+        public void UpdateQuality_WhenNoLegendaryItem_SellInDateDecrease(string itemName)
+        {
+            // Arrange
+            IList<Item> Items = new List<Item> { new Item { Name = itemName, SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(-1, Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void UpdateQuality_WhenLegendaryItem_SellInDateRemains(int sellInDate)
+        {
+            // Arrange
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellInDate, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(sellInDate, Items[0].SellIn);
         }
     }
 }
