976d297133171d5a1cf01b2b52a0ed605f83bc1f
Félix Díez <felixeu31@gmail.com>
2023-05-07 09:46:56 +0200

    refator: change to foreach

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -12,9 +12,8 @@ namespace GildedRoseKata
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach(var item in Items)
             {
-                var item = Items[i];
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (item.Quality > 0)
