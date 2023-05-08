15c770cfb6665b9439dcc9045e84218f6a916534
Félix Díez Arias <fdiez@tunstall.es>
2023-05-08 17:07:45 +0200

    refactor: extrac methods

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -23,75 +23,89 @@ namespace GildedRoseKata
         {
             if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
-                return;
-            }
 
-            if (item.Name == "Aged Brie")
+            }
+            else if (item.Name == "Aged Brie")
+            {
+                UpdateAgedBrieQuality(item);
+            }
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                UpdateBackStagePassesQuality(item);
+            }
+            else
+            {
+                UpdateNormalItemQuality(item);
+            }
+        }
+
+        private static void UpdateNormalItemQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                if (item.SellIn < 0)
+                {
+                    if (item.Quality > 1)
+                    {
+                        item.Quality = item.Quality - 2;
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+                else
+                {
+                    if (item.Quality > 0)
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+        }
+
+        private static void UpdateBackStagePassesQuality(Item item)
+        {
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+            else if (item.SellIn < 6)
+            {
+                if (item.Quality < 48)
+                {
+                    item.Quality = item.Quality + 3;
+                }
+                else
+                {
+                    item.Quality = 50;
+                }
+            }
+            else if (item.SellIn < 11)
+            {
+                if (item.Quality < 49)
+                {
+                    item.Quality = item.Quality + 2;
+                }
+                else
+                {
+                    item.Quality = 50;
+                }
+            }
+            else
             {
                 if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
                 }
             }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+        }
+
+        private static void UpdateAgedBrieQuality(Item item)
+        {
+            if (item.Quality < 50)
             {
-                if (item.SellIn < 0)
-                {
-                    item.Quality = 0;
-                }
-                else if (item.SellIn < 6)
-                {
-                    if (item.Quality < 48)
-                    {
-                        item.Quality = item.Quality + 3;
-                    }
-                    else
-                    {
-                        item.Quality = 50;
-                    }
-                }
-                else if (item.SellIn < 11)
-                {
-                    if (item.Quality < 49)
-                    {
-                        item.Quality = item.Quality + 2;
-                    }
-                    else
-                    {
-                        item.Quality = 50;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 1)
-                        {
-                            item.Quality = item.Quality - 2;
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                }
+                item.Quality = item.Quality + 1;
             }
         }
 
