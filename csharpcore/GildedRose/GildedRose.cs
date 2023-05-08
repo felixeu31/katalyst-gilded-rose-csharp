41b6e60b04816c0a4e4db457042776e8f40b1a7c
Félix Díez Arias <fdiez@tunstall.es>
2023-05-08 16:52:35 +0200

    fix: aged brie continue degrading at same rate after sellin date

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -62,14 +62,7 @@ namespace GildedRoseKata
 
             if (item.SellIn < 0)
             {
-                if (item.Name == "Aged Brie")
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-                else
+                if (item.Name != "Aged Brie")
                 {
                     if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
